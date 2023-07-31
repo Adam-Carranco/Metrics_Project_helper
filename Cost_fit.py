1614f41c0e61e882c541b38afbc579b7149e8b0f
Adam Carranco <112434691+Adam-Carranco@users.noreply.github.com>
2023-07-31 10:31:05 -0600

    Update Cost_fit.py

--- Cost_fit.py
+++ Cost_fit.py
@@ -10,8 +10,8 @@ def plot_linear_best_fit(df):
     # Remove rows with None values in the "Pass/Fail Status.1" column
     df = df.dropna(subset=["Pass/Fail Status.1"])
 
-    # Prepare the input features and target variable
-    y = df["Pass/Fail Status.1"]
+    # Prepare the target variable
+    y = df["Total Incremental Cost"]
 
     # Iterate through each column (excluding "Pass/Fail Status.1") and plot it against "Total Incremental Cost"
     for column in df.columns[1:]:
@@ -31,28 +31,37 @@ def plot_linear_best_fit(df):
             plt.plot(X, y_pred, color="r", label=f"Linear Fit (R-squared: {r_squared:.2f})")
 
             plt.xlabel(column)
-            plt.ylabel("Pass/Fail Status")
-            plt.title(f"{column} vs. Pass/Fail Status")
+            plt.ylabel("Total Incremental Cost")
+            plt.title(f"{column} vs. Total Incremental Cost")
             plt.legend()
             plt.show()
 
-    # Plot "Total Incremental Cost" against the "Pass/Fail Status.1" metric
-    X_total_inc_cost = df[["Total Incremental Cost"]]
+# Plot "Total Incremental Cost" against the "Pass/Fail Status.1" metric
+def plot_total_cost_vs_pass_fail(df):
+    # Convert the "Pass/Fail Status.1" column to a binary format (1 for pass, 0 for fail)
+    df["Pass/Fail Status.1"] = df["Pass/Fail Status.1"].replace({3: 1, 2: None, 1: 0})
 
-    # Fit a linear regression model for "Total Incremental Cost" against the target variable
-    linear_model_total_cost = LinearRegression()
-    linear_model_total_cost.fit(X_total_inc_cost, y)
+    # Remove rows with None values in the "Pass/Fail Status.1" column
+    df = df.dropna(subset=["Pass/Fail Status.1"])
+
+    # Prepare the input features and target variable
+    X = df[["Pass/Fail Status.1"]]
+    y = df["Total Incremental Cost"]
+
+    # Fit a linear regression model
+    linear_model = LinearRegression()
+    linear_model.fit(X, y)
 
     # Calculate the correlation coefficient (R-squared) between predicted and actual values
-    y_pred_total_cost = linear_model_total_cost.predict(X_total_inc_cost)
-    r_squared_total_cost = pearsonr(y, y_pred_total_cost)[0] ** 2
+    y_pred = linear_model.predict(X)
+    r_squared = pearsonr(y, y_pred)[0] ** 2
 
     # Plot the data and the linear regression line
-    plt.scatter(X_total_inc_cost, y, color="b", label="Data")
-    plt.plot(X_total_inc_cost, y_pred_total_cost, color="r", label=f"Linear Fit (R-squared: {r_squared_total_cost:.2f})")
+    plt.scatter(X, y, color="b", label="Data")
+    plt.plot(X, y_pred, color="r", label=f"Linear Fit (R-squared: {r_squared:.2f})")
 
-    plt.xlabel("Total Incremental Cost")
-    plt.ylabel("Pass/Fail Status")
+    plt.xlabel("Pass/Fail Status")
+    plt.ylabel("Total Incremental Cost")
     plt.title("Total Incremental Cost vs. Pass/Fail Status")
     plt.legend()
     plt.show()
@@ -60,3 +69,4 @@ def plot_linear_best_fit(df):
 # Example usage:
 # Assuming your DataFrame is named "data_frame"
 plot_linear_best_fit(data_frame)
+plot_total_cost_vs_pass_fail(data_frame)
